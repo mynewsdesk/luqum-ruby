b7841eee1a0d65c9a66c9c2ed9f262abddd5e7c2
David Backeus <duztdruid@gmail.com>
2026-04-22 13:29:13 +0200

    Split item factory spec from visitor spec

--- spec/elasticsearch/visitor_spec.rb
+++ spec/elasticsearch/visitor_spec.rb
@@ -1117,23 +1117,3 @@ RSpec.describe Luqum::Elasticsearch::Visitor::ElasticsearchQueryBuilder do
     end
   end
 end
-
-RSpec.describe Luqum::Elasticsearch::Tree::ElasticSearchItemFactory do
-  it "lets explicit field_options override factory defaults" do
-    factory = described_class.new(
-      [],
-      {},
-      { "foo" => { "match_type" => "phrase" } },
-    )
-
-    word = factory.build(Luqum::Elasticsearch::Visitor::EWord, "bar")
-    expect(word.field_options).to eq({ "foo" => { "match_type" => "phrase" } })
-
-    word = factory.build(
-      Luqum::Elasticsearch::Visitor::EWord,
-      "bar",
-      field_options: { "foo" => { "match_type" => "term" } },
-    )
-    expect(word.field_options).to eq({ "foo" => { "match_type" => "term" } })
-  end
-end
